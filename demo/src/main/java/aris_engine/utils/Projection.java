9ff49cf04a06b9407f164ba6dcd7e6c805006127
Hieu1223 <minhhieunn2000@gmail.com>
2025-02-26 01:05:39 +0700

    Something wrong with the rotation

--- demo/src/main/java/aris_engine/utils/Projection.java
+++ demo/src/main/java/aris_engine/utils/Projection.java
@@ -2,9 +2,23 @@ package aris_engine.utils;
 
 public class Projection {
     public static float[] OrthoGraphic(double aspect, double near, double far, double fov){
-        
+        double temp = Math.tan(fov/2);
+        float[] res = new float[16];
+        res[0] = (float)(1/aspect/temp);
+        res[5] = (float)(1/temp);
+        res[10] = (float)((-near - far)/(far-near));
+        res[11] = (float)((2* near * far)/(far-near));
+        res[14] = -1;
+        return res;
     }
     public static float[] Perspective(double aspect, double near, double far, double fov){
-
+        double temp = Math.tan(fov/2);
+        float[] res = new float[16];
+        res[0] = (float)(1/aspect/temp);
+        res[5] = (float)(1/temp);
+        res[10] = (float)((-near - far)/(far-near));
+        res[11] = (float)((2* near * far)/(far-near));
+        res[14] = -1;
+        return res;
     }
 }
